6242e8feb0c8fad46ebf2d49cbe8ac1fc51bde4e
Edwin Schaeffer <ershf2@gmail.com>
2023-02-02 02:07:13 -0600

    Initial commit

---
+++ demo/src/main/java/com/example/demo/Is296DemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Is296DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Is296DemoApplication.class, args);
+	}
+
+}
