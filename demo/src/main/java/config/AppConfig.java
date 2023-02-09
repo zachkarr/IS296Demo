a7fb80882c202c346577f1984c4ebb99e01eed82
ES <ershf2@gmail.com>
2023-02-09 02:05:12 -0600

    Java Config examples

--- demo/src/main/java/config/AppConfig.java
+++ demo/src/main/java/config/AppConfig.java
@@ -2,9 +2,11 @@ package config;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 @ComponentScan("com.example")
+@Import({AppConfigJavaConfig.class})
 public class AppConfig {
 
 	
